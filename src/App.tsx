01a95ec56c2e5adb7f19c594cf1f4424f53d8210
Daniel Valberg <danielevalberg@gmail.com>
2020-05-29 22:12:51 +0000

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,7 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import ContactUs from "./pages/ContactUs";
 import AddShow from "./pages/AddShow";
-<<<<<<< HEAD
 import Shows from "./pages/Shows";
-=======
->>>>>>> f3062854598024edf727f2567d5ec873cd39cf70
 
 function App() {
 	return (
@@ -21,12 +18,9 @@ function App() {
 				<Route path="/add-show">
 					<AddShow />
 				</Route>
-<<<<<<< HEAD
 				<Route path="/shows/:id">
 					<Shows />
 				</Route>
-=======
->>>>>>> f3062854598024edf727f2567d5ec873cd39cf70
 				<Route path="/">
 					<Home />
 				</Route>
